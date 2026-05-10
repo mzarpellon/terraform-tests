8bf1031e5ce8c4ce047fbfd338c508fbdbee92ee
mzarpellon <michael.zapelao@gmail.com>
2026-05-10 19:25:55 -0300

    adicionado estrutura do schema

--- scripts/processar_transacoes.py
+++ scripts/processar_transacoes.py
@@ -4,6 +4,7 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 import pyspark.sql.functions as F
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
 
 # Captura variáveis de Ambiente no Terraform
 args = getResolvedOptions(sys.argv, ['JOB_NAME', 'S3_SOURCE', 'S3_TARGET'])
@@ -14,8 +15,19 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
+# schema
+schema = StructType([
+    StructField("id_transacao", StringType(), True),
+    StructField("valor", DoubleType(), True),
+    StructField("data_hora", StringType(), True), # Ou TimestampType se o formato permitir
+    StructField("cliente_id", StringType(), True),
+    StructField("status", StringType(), True)
+])
+
 # Leitura do arquivo JSON (Raw)
-df = spark.read.json(args['S3_SOURCE'])
+df = spark.read \
+    .schema(schema) \
+    .json(args['S3_SOURCE'])
 
 # Transformação simples 
 df_refined = df.withColumn('status_processamento', F.lit('CONCLUIDO'))
