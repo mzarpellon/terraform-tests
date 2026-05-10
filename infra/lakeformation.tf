433c5684834c67d6f1a7fa2bdc1edb12812bd93e
mzarpellon <michael.zapelao@gmail.com>
2026-05-10 17:57:14 -0300

    Ajustado lakeformation tf

--- infra/lakeformation.tf
+++ infra/lakeformation.tf
@@ -1,6 +1,16 @@
-# Registrar o bucket no Lake Formation
-resource "aws_lakeformation_resource" "data_location" {
-  arn = aws_s3_bucket.bucket.arn
+# Registro do Bucket RAW
+resource "aws_lakeformation_resource" "raw_location" {
+  arn = aws_s3_bucket.raw.arn
+}
+
+# Registro do Bucket REFINED
+resource "aws_lakeformation_resource" "refined_location" {
+  arn = aws_s3_bucket.refined.arn
+}
+
+# Registro do Bucket CURATED
+resource "aws_lakeformation_resource" " curated_location" {
+  arn = aws_s3_bucket.curated.arn
 }
 
 # Conceder permissões ao Glue Role
